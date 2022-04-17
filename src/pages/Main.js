924f370d8e1038947d4780fdd4fb4b94e150a709
shon97 <wjdgns200297@gmail.com>
2022-04-18 01:01:14 +0900

    ADD: jsconfig.json, env-cross

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import List from "../components/List";
+import List from "components/List";
 
 import styled from "styled-components";
 
